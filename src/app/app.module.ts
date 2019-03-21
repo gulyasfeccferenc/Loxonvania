30741219c93428687a4b776f57e5f3bb9346dc1f
Ferenc Gulyas <ferenc.gulyas@loxon.eu>
2019-03-21 15:46:05 +0100

    Added basic routing to main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { WorkplaceComponent } from './workplace/workplace.component';
+import { AchievmentsComponent } from './achievments/achievments.component';
+
+const appRoutes: Routes = [
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  { path: '',
+    component: DashboardComponent
+  },
+  { path: 'workplace',
+    component: WorkplaceComponent
+  },
+  { path: 'achievments',
+    component: AchievmentsComponent
+  },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    WorkplaceComponent,
+    AchievmentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
