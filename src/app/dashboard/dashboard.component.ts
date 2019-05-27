bac91b34b138505f67345f13dd21010192907a3d
Ferenc Gulyas <ferenc.gulyas@loxon.eu>
2019-05-27 18:23:22 +0200

    Added carousel and work on dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../models/auth/user.model';
+import {Level} from '../models/level/level.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.sass']
 })
 export class DashboardComponent implements OnInit {
+  leaders = [
+    {name: 'Tuk Peregrin', level: 100, points: 1000, company: 'Répa & Co'},
+    {name: 'Csavardi Samu', level: 100, points: 1000, company: 'Csűrös és Társa'},
+    {name: 'Borbak Trufa', level: 100, points: 1000, company: 'Csülök Bt'}
+  ];
+  slides = [
+    {src: '/assets/tutorial_01.jpg'}
+  ]
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
